2b6a50281e0365f111146754a7129881f4033d38
zack <zack-ji@live.cn>
2019-08-19 17:20:40 +0800

    BillQuery模型变动

--- Models/BillQuery/Column.cs
+++ Models/BillQuery/Column.cs
@@ -2,11 +2,10 @@
 
 namespace ApiSite.Models.BillQuery {
     public class Column {
-        public string Title { get; set; }
-        public Dictionary<int, string> Menu { get; set; }
-        public int Width { get; set; }
         public string Key { get; set; }
+        public string Title { get; set; }
         public bool Sortable { get; set; }
-        public bool Fixed { get; set; }
+        public int Width { get; set; }
+        public Dictionary<int, string> Menu { get; set; }
     }
 }
--- Models/BillQuery/ColumnInfo.cs
+++ Models/BillQuery/ColumnInfo.cs
@@ -9,23 +9,19 @@ namespace ApiSite.Models.BillQuery {
         #region Public Properties
 
         [Key] [Required] public string Key { get; set; }
-
         [Required] public virtual string Title { get; set; }
         public virtual IEnumerable<FieldMenuItem> Menu { get; set; }
         public bool Sortable { get; set; }
-        public bool Fixed { get; set; }
-
         public int Width { get; set; }
 
         [NotMapped]
         public Column Result =>
             new Column {
-                Title = Title,
-                Menu = Menu.ToDictionary(m => m.Id, m => m.Title),
                 Key = Key,
+                Title = Title,
+                Sortable = Sortable,
                 Width = Width,
-                Fixed = Fixed,
-                Sortable = Sortable
+                Menu = Menu.ToDictionary(m => m.Id, m => m.Title),
             };
 
         #endregion Public Properties
